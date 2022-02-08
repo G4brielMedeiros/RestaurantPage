58c1ede74b0dc9a2aa294c9814b9bbad851b1e98
Gabriel Medeiros <G4brielMedeiros@pm.me>
2022-02-08 16:53:00 -0300

    [clean] homePage code

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -3,22 +3,13 @@ import breadImg from '../images/breadsByPixabay.jpg';
 export default function getHomePage() {
   const homePage = document.createElement("div");
 
-  const title = document.createElement("h1");
-  const desc1 = document.createElement("h2");
-  const desc2 = document.createElement("h2");
-  const desc3 = document.createElement("h2");
+  const title = generateElement("h1", "title", "The Bakery");
+  const desc1 = generateElement("h2", "description", "This is the place that sells bread");
+  const desc2 = generateElement("h2", "description", "We're very excited to offer you tons of bread, and a little butter to go along");
+  const desc3 = generateElement("h2", "description", "This bread place has been in business for over 15000 years. That's right, we're older than civilization.");
+  
   const bread = new Image();
-
-  title.textContent = "The Bakery";
-  desc1.textContent = "This is the place that sells bread";
-  desc2.textContent = "We're very excited to offer you tons of bread, and a little butter to go along";
-  desc3.textContent = "This bread place has been in business for over 15000 years. That's right, we're older than civilization. We invented bread, so shut up and buy";
   bread.src = breadImg;
-
-  title.classList = "title";
-  desc1.classList = "description";
-  desc2.classList = "description";
-  desc3.classList = "description";
   bread.classList = "background";
 
   homePage.appendChild(title);
@@ -28,4 +19,13 @@ export default function getHomePage() {
   homePage.appendChild(bread);
 
   return homePage;
+}
+
+function generateElement(documentType, classList, textContent) {
+  const element = document.createElement(documentType);
+  element.textContent = textContent;
+  element.classList = classList;
+
+  return element;
+
 }
